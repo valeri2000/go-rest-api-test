a73f31f329f94ac8f61cf0be4c1900de5fe10cee
Valeri Stanchev <stan4ev6@gmail.com>
2021-08-13 12:18:52 +0300

    update memdb methods' signature

--- memdb/student_store.go
+++ memdb/student_store.go
@@ -1,24 +1,28 @@
 package memdb
 
-import "main.go/student"
+import (
+	"errors"
+
+	"main.go/student"
+)
 
 type Repo struct {
 	Students []student.Student `json:"students"`
 }
 
-func (repo *Repo) GetStudents() []student.Student {
+func (repo *Repo) GetStudents() ([]student.Student, error) {
 	data := make([]student.Student, repo.CountStudents())
 	copy(data, repo.Students)
-	return data
+	return data, nil
 }
 
-func (repo *Repo) GetStudentByID(id string) student.Student {
+func (repo *Repo) GetStudentByID(id string) (student.Student, error) {
 	for _, student := range repo.Students {
 		if student.Id == id {
-			return student
+			return student, nil
 		}
 	}
-	return student.Student{}
+	return student.Student{}, errors.New("couldn't get student id")
 }
 
 func (repo *Repo) CountStudents() int {
@@ -34,24 +38,37 @@ func (repo *Repo) HasStudentWithID(id string) bool {
 	return false
 }
 
-func (repo *Repo) AddStudent(student student.Student) {
+func (repo *Repo) AddStudent(student student.Student) error {
 	repo.Students = append(repo.Students, student)
+	return nil
 }
 
-func (repo *Repo) DeleteStudentByID(id string) {
+func (repo *Repo) DeleteStudentByID(id string) error {
+	ok := false
 	for i, student := range repo.Students {
 		if student.Id == id {
 			repo.Students = append(repo.Students[:i], repo.Students[i+1:]...)
+			ok = true
 			break
 		}
 	}
+	if !ok {
+		return errors.New("couldn't delete student")
+	}
+	return nil
 }
 
-func (repo *Repo) UpdateStudentByID(id string, updatedStudent student.Student) {
+func (repo *Repo) UpdateStudentByID(id string, updatedStudent student.Student) error {
+	ok := false
 	for i, student := range repo.Students {
 		if student.Id == id {
 			repo.Students[i] = updatedStudent
+			ok = true
 			break
 		}
 	}
+	if !ok {
+		return errors.New("couldn't update student")
+	}
+	return nil
 }
